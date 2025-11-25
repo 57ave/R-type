41e9948f51c965081e5876ceeeb383e96c5151fc
57ave <gustave.delecroix@epitech.eu>
2025-11-25 17:58:44 +0100

    feat(ecs): add ecs namespace

---
+++ game_engine/ecs/include/ECS.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "Types.hpp"
+#include "EntityManager.hpp"
+#include "ComponentArray.hpp"
+#include "ComponentManager.hpp"
+#include "System.hpp"
+#include "SystemManager.hpp"
+#include "Coordinator.hpp"
