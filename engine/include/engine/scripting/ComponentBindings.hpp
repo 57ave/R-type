91f66881a1e83f49ec8d325e423619c8c95becab
57ave <gustave.delecroix@epitech.eu>
2025-12-15 22:36:24 +0100

    feat: add scripting and config files

---
+++ engine/include/engine/scripting/ComponentBindings.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "LuaState.hpp"
+#include "../ecs/Coordinator.hpp"
+#include "../ecs/Components.hpp"
+#include <sol/sol.hpp>
+
+namespace Scripting {
+
+    /**
+     * @brief ComponentBindings - Exposes ECS components to Lua
+     * 
+     * This class provides bindings between C++ ECS components and Lua scripts.
+     * Components themselves are defined in engine/ecs/Components.hpp
+     * 
+     * Usage:
+     *   sol::state lua;
+     *   ComponentBindings::RegisterAll(lua);
+     *   ComponentBindings::RegisterCoordinator(lua, &coordinator);
+     */
+    class ComponentBindings {
+     public:
+            static void RegisterAll(sol::state& lua);
+            
+            // Register individual component types
+            static void RegisterTransform(sol::state& lua);
+            static void RegisterVelocity(sol::state& lua);
+            static void RegisterSprite(sol::state& lua);
+            static void RegisterHealth(sol::state& lua);
+            static void RegisterDamage(sol::state& lua);
+            static void RegisterAIController(sol::state& lua);
+            static void RegisterCollider(sol::state& lua);
+            static void RegisterPlayer(sol::state& lua);
+            static void RegisterEnemy(sol::state& lua);
+            static void RegisterProjectile(sol::state& lua);
+            static void RegisterPowerUp(sol::state& lua);
+            
+            // Register ECS coordinator bindings
+            static void RegisterCoordinator(sol::state& lua, ECS::Coordinator* coordinator);
+    };
+
+} // namespace Scripting
